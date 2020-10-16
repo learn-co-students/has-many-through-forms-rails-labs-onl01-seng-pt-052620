b5594d4b291c58162f8c97da4d0e421cc0c69c47
rumikoacopa <rumikorios@gmail.com>
2020-10-15 23:12:43 -0400

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
-
+  def user_attributes=(attributes)
+    self.user = User.find_or_create_by(attributes) unless attributes[:username].blank?
+  end
 end
