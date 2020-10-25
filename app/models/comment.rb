3d7195a732121edfb356fbc5c3e6c7934a27ef2f
Capsulecoding <capsulecoding@icloud.com>
2020-10-25 05:45:58 -0700

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,18 @@
+
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+
+  def user_attributes
+    @username
+  end
+
+  def user_attributes=(user)
+    # byebug
+    unless user[:username] == ""
+      self.user = User.find_or_create_by(username: user[:username])
+      self.user.update(user)
+    end
+  end
 
 end
