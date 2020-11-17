1344689585179ff1eccae951c773182b608ddd3b
edmundo papi <edmundopierre@yahoo.com>
2020-11-17 03:33:19 +0000

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,12 @@
+# class Comment < ActiveRecord::Base
+#   belongs_to :user
+#   belongs_to :post
+# accepts_nested_attrbutes_for :user, reject_if: :all_blank
+# end
+
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  accepts_nested_attributes_for :user, reject_if: :all_blank
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,32 @@
+# class Post < ActiveRecord::Base
+#   has_many :post_categories
+#   has_many :categories, through: :post_categories
+#   has_many :comments
+#   has_many :users, through: :comments
+# accepts_nested_attrbutes_for :categories, reject_if: :all_blank
+#
+# def comments_attributes=(comments_attributes)
+# comments_attributes.value.each do |comments_attribute|
+#   comment = Comment.find_or_create_by(comment_attribute)
+#   self.comment_attribute.build(ccomment: comment)
+# end
+#
+# end
+#
+# end
+
 class Post < ActiveRecord::Base
   has_many :post_categories
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  accepts_nested_attributes_for :categories, reject_if: :all_blank
 
 
+  def comments_attributes=(comment_attributes)
+    comment_attributes.values.each do |comment_attribute|
+      comment = Comment.find_or_create_by(comment_attribute)
+      self.comment_attributes.build(comment: comment)
+    end
+  end
 end
